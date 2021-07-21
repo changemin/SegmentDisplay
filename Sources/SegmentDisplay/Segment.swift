a46c56c161737c9b1670bf56146436aab3e27d49
Changemin <chgemn@gmail.com>
2021-07-21 10:50:56 +0900

    Adjust Modulus

--- Sources/SegmentDisplay/Segment.swift
+++ Sources/SegmentDisplay/Segment.swift
@@ -9,18 +9,70 @@ import SwiftUI
 
 public struct Segment: View {
     @Binding var num: Int
-    var k: CGFloat = 50
+    var width: CGFloat = 50
+    var color: Color = .red
+    @State var segment: [Bool] = [ false, false, false, false, false, false, false ]
+    var spaceMod: CGFloat = 0.05
     public var body: some View {
         ZStack {
-            RoundSquare().fill(Color.green).frame(width: k, height: k*0.25) // 4
-            RoundSquare().fill(Color.green).frame(width: k*0.25, height: k*0.75-2).offset(x: -k*0.375, y: -k*0.5) // 2
-            RoundSquare().fill(Color.green).frame(width: k*0.25, height: k*0.75-2).offset(x: -k*0.375, y: k*0.5) // 5
-            RoundSquare(topRight: k).fill(Color.green).frame(width: k*0.25, height: k-2).offset(x: k*0.375, y: -k*0.625-1) // 3
-            RoundSquare(bottomRight: k).fill(Color.green).frame(width: k*0.25, height: k-2).offset(x: k*0.375, y: k*0.625+1) // 6
-            RoundSquare(topLeft: k).fill(Color.green).frame(width: k*0.75-1, height: k*0.25).offset(x: -k*0.125-1, y: -k) // 1
-            RoundSquare(bottomLeft: k).fill(Color.green).frame(width: k*0.75-1, height: k*0.25).offset(x: -k*0.125-1, y: k) // 7
+            RoundSquare().fill(Color.green.opacity(segment[3] ? 1 : 0.3))
+                .frame(width: width, height: width*0.25) // 4
+            
+            RoundSquare().fill(Color.green.opacity(segment[1] ? 1 : 0.3))
+                .frame(width: width*0.25, height: width*0.75-width*spaceMod*2)
+                .offset(x: -width*0.375, y: -width*0.5) // 2
+            
+            RoundSquare().fill(Color.green.opacity(segment[4] ? 1 : 0.3))
+                .frame(width: width*0.25, height: width*0.75-width*spaceMod*2)
+                .offset(x: -width*0.375, y: width*0.5) // 5
+            
+            RoundSquare(topRight: width).fill(Color.green.opacity(segment[2] ? 1 : 0.3))
+                .frame(width: width*0.25, height: width-width*spaceMod)
+                .offset(x: width*0.375, y: -width*0.625-width*spaceMod*0.5) // 3
+            
+            RoundSquare(bottomRight: width).fill(Color.green.opacity(segment[5] ? 1 : 0.3))
+                .frame(width: width*0.25, height: width-width*spaceMod)
+                .offset(x: width*0.375, y: width*0.625+width*0.025) // 6
+            
+            RoundSquare(topLeft: width).fill(Color.green.opacity(segment[0] ? 1 : 0.3))
+                .frame(width: width*0.75-width*spaceMod*0.5, height: width*0.25)
+                .offset(x: -width*0.125-width*spaceMod*0.5, y: -width) // 1
+            
+            RoundSquare(bottomLeft: width).fill(Color.green.opacity(segment[6] ? 1 : 0.3))
+                .frame(width: width*0.75-width*spaceMod*0.5, height: width*0.25)
+                .offset(x: -width*0.125-width*spaceMod*0.5, y: width) // 7
 
-        }.frame(width: k, height: k*2.25)
+        }.frame(width: width, height: width*2.25)
+        .onChange(of: num, perform: { value in
+            update()
+        })
+    }
+    
+    private func update() {
+        switch num {
+        case 0:
+            self.segment = [true, true, true, false, true, true, true]
+        case 1:
+            self.segment = [false, false, true, false, false, true, false]
+        case 2:
+            self.segment = [true, false, true, true, true, false, true]
+        case 3:
+            self.segment = [true, false, true, true, false, true, true]
+        case 4:
+            self.segment = [false, true, true, true, false, true, false]
+        case 5:
+            self.segment = [true, true, false, true, false, true, true]
+        case 6:
+            self.segment = [true, true, false, true, true, true, true]
+        case 7:
+            self.segment = [true, true, true, false, false, true, false]
+        case 8:
+            self.segment = [true, true, true, true, true, true, true]
+        case 9:
+            self.segment = [true, true, true, true, false, true, false]
+        default:
+            self.segment = [false, false, false, false, false, false, false]
+        }
     }
     public init(num: Binding<Int>) {
         self._num = num
